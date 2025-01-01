673b8d52cd88efca1ac46563f4abbaa80668598a
Daniel Blancas <edblancas@gmail.com>
2025-01-01 12:06:26 -0600

    test playground

--- src/main.lua
+++ src/main.lua
@@ -4,4 +4,18 @@ function M.add(a, b)
   return a + b
 end
 
+function M:test(foo)
+  print('self=', self, 'foo=', foo)
+end
+
+-- is like lua inserts the 2 arg before foo, so self is 2
+M.test(2)
+M:test(3)
+
+-- just one slot between 1: and slot
+print("1: ", table.unpack({'hello', 'world', '!'}), '<lost>')
+
 return M
+
+-- throws error
+-- print('this is not printed')
